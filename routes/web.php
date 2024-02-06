713371b8e6f97d2d1cfbefb64a833d5fd9885b79
Francislie <mfrancislie@gmail.com>
2024-02-06 11:45:33 -0800

    model and reading data

--- routes/web.php
+++ routes/web.php
@@ -15,76 +15,24 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
-
-
 Route::get('/', function() {
     return redirect()->route('tasks.index');
 });
 
-// function is anonymous so i need to use use($tasks)
-Route::get('/tasks', function () use ($tasks) {
+// latest()->get(). you will get the most recent task first
+// SQL queries in an object oriented way.
+// App\Models\Task::all()->latest()where('completed, true')->get(). you will get all tasks completed 
+// cmd: php artisan tinker is lets you write queries.
+
+
+// fetching all the data from the database
+Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => $tasks
-    ]);
+        'tasks' => App\Models\Task::all()->latest()->get()
+        ]);
 })->name('tasks.index');
 
-Route::get('/tasks/{id}', function ($id) use ($tasks) {
- $task = collect($tasks)->firstWhere('id', $id);
-
- if(!$task){
-    abort(Response::HTTP_NOT_FOUND);
- }
-    return view('singletask', ['task' => $task]);
+// to render a single task
+Route::get('/tasks/{id}', function ($id) { 
+    return view('singletask', ['task' => App\Models\Task::findOrFail($id)]);
 })->name('tasks.singletask');
