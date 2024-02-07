489d6e7e9548f5dc305d55c747de9023683bdee2
Francislie <mfrancislie@gmail.com>
2024-02-06 21:03:47 -0800

    form and csrf protection

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
 
@@ -23,16 +24,24 @@ Route::get('/', function() {
 // SQL queries in an object oriented way.
 // App\Models\Task::all()->latest()where('completed, true')->get(). you will get all tasks completed 
 // cmd: php artisan tinker is lets you write queries.
-
+// cmd: php artisan route:list. show all the routes list
 
 // fetching all the data from the database
 Route::get('/tasks', function () {
     return view('index', [
-        'tasks' => App\Models\Task::all()->latest()->get()
+        'tasks' => App\Models\Task::latest()->get()
         ]);
 })->name('tasks.index');
+
+Route::view('/tasks/{create}', 'create')->name('tasks.create');
 
 // to render a single task
 Route::get('/tasks/{id}', function ($id) { 
     return view('singletask', ['task' => App\Models\Task::findOrFail($id)]);
 })->name('tasks.singletask');
+
+
+Route::post('/tasks', function(Request $request){
+//    dd('we have reached the store route'); for testing
+dd($request->all());
+})->name('tasks.store');
